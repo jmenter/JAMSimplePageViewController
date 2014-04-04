ca8a0e5c21b22a445b2810b401aaf9ae7793b122
Jeff Menter <jmenter@Jeffs-MacBook-Pro-2.local>
2014-04-04 14:06:54 -0500

    improved background of example view controller

--- JAMSimplePageViewController/JAMNumberedViewController.m
+++ JAMSimplePageViewController/JAMNumberedViewController.m
@@ -3,9 +3,15 @@
 
 @interface JAMNumberedViewController ()
 @property (nonatomic) NSNumber *number;
+@property (nonatomic) CAGradientLayer *backgroundGradient;
 @end
 
 @implementation JAMNumberedViewController
+
+CGFloat randomFloat(CGFloat maximum)
+{
+    return ((CGFloat)arc4random() / (CGFloat)4294967296) * maximum;
+}
 
 - (instancetype)initWithNumber:(NSNumber *)number;
 {
@@ -17,12 +23,28 @@
 
 - (void)viewDidLoad;
 {
+    self.backgroundGradient = CAGradientLayer.new;
+    self.backgroundGradient.colors = @[ (id)[UIColor colorWithHue:randomFloat(1)
+                                                       saturation:randomFloat(1)
+                                                       brightness:randomFloat(1)
+                                                            alpha:randomFloat(1)].CGColor,
+                                        (id)[UIColor colorWithHue:randomFloat(1)
+                                                       saturation:randomFloat(1)
+                                                       brightness:randomFloat(1)
+                                                            alpha:randomFloat(1)].CGColor];
+    [self.view.layer addSublayer:self.backgroundGradient];
+    
     UILabel *numberLabel = [UILabel.alloc initWithFrame:self.view.frame];
     numberLabel.text = self.number.description;
     numberLabel.textAlignment = NSTextAlignmentCenter;
     numberLabel.font = [UIFont boldSystemFontOfSize:144];
+    numberLabel.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
     [self.view addSubview:numberLabel];
-    self.view.backgroundColor = UIColor.lightGrayColor;
+}
+
+- (void)viewWillLayoutSubviews;
+{
+    self.backgroundGradient.frame = self.view.frame;
 }
 
 @end
